4fc3abaa257da4d9f3ee12f8fcd65c1296645d3e
yuulye <yuulye@gmail.com>
2023-05-25 11:47:41 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# game_rupiah
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/game_rupiah/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.game_rupiah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
